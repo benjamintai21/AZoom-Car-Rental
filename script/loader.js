adfeb17cae467ee1b21c263a3d5e4e97a5497327
benjamintai21 <benjamintai21@gmail.com>
2024-10-29 17:56:46 +0800

    Delete script directory

--- script/loader.js
+++
@@ -1,8 +0,0 @@
-function loader() {
-    window.addEventListener("load", () => {     
-    document.querySelector(".loader").classList.add("loader--hidden");
-    document.querySelector(".loader").addEventListener("transitionend", () => {
-    // document.body.removeChild(document.querySelector(".loader"));
-    });
-    });
-}
